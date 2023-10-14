d547bf23d20e77bb09c9636266c0b83ab6aaa796
chethankumarkv123 <86190454+chethankumarkv123@users.noreply.github.com>
2023-10-14 17:59:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,381 @@
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const closeBtn = document.querySelector(".close-btn");
-const chatbox = document.querySelector(".chatbox");
-const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
+const userMessage = [
 
-let userMessage = null; // Variable to store user's message
-const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
-const inputInitHeight = chatInput.scrollHeight;
+  ["hi", "hey", "hello"],
 
-const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
+  ["sure", "yes", "no"],
+
+  ["are you genious", "are you nerd", "are you intelligent"],
+
+  ["i hate you", "i dont like you"],
+
+  ["how are you", "how is life", "how are things", "how are you doing"],
+
+  ["how is corona", "how is covid 19", "how is covid19 situation"],
+
+  ["what are you doing", "what is going on", "what is up"],
+
+  ["how old are you"],
+
+  ["who are you", "are you human", "are you bot", "are you human or bot"],
+
+  ["who created you", "who made you", "who is your creator"],
+
+ 
+
+  [
+
+    "your name please",
+
+    "your name",
+
+    "may i know your name",
+
+    "what is your name",
+
+    "what call yourself"
+
+  ],
+
+  ["i love you"],
+
+  ["happy", "good", "fun", "wonderful", "fantastic", "cool", "very good"],
+
+  ["bad", "bored", "tired"],
+
+  ["help me", "tell me story", "tell me joke"],
+
+  ["ah", "ok", "okay", "nice", "welcome"],
+
+  ["thanks", "thank you"],
+
+  ["what should i eat today"],
+
+  ["bro"],
+
+  ["what", "why", "how", "where", "when"],
+
+  ["corona", "covid19", "coronavirus"],
+
+  ["you are funny"],
+
+  ["i dont know"],
+
+  ["boring"],
+
+  ["im tired"]
+
+];
+
+const botReply = [
+
+  ["Hello!", "Hi!", "Hey!", "Hi there!"],
+
+  ["Okay"],
+
+  ["Yes I am! "],
+
+  ["I'm sorry about that. But I like you dude."],
+
+  [
+
+    "Fine... how are you?",
+
+    "Pretty well, how are you?",
+
+    "Fantastic, how are you?"
+
+  ],
+
+  ["Getting better. There?", "Somewhat okay!", "Yeah fine. Better stay home!"],
+
+ 
+
+  [
+
+    "Nothing much",
+
+    "About to go to sleep",
+
+    "Can you guess?",
+
+    "I don't know actually"
+
+  ],
+
+  ["I am always young."],
+
+  ["I am just a bot", "I am a bot. What are you?"],
+
+  ["chethan Mr | Nytro"],
+
+  ["I am nameless", "I don't have a name"],
+
+  ["I love you too", "Me too"],
+
+  ["Have you ever felt bad?", "Glad to hear it"],
+
+  ["Why?", "Why? You shouldn't!", "Try watching TV", "Chat with me."],
+
+  ["What about?", "Once upon a time..."],
+
+  ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+
+  ["You're welcome"],
+
+  ["Briyani", "Burger", "Sushi", "Pizza"],
+
+  ["Dude!"],
+
+  ["Yes?"],
+
+  ["Please stay home"],
+
+  ["Glad to hear it"],
+
+  ["Say something interesting"],
+
+  ["Sorry for that. Let's chat!"],
+
+  ["Take some rest, Dude!"]
+
+];
+
+ 
+
+const alternative = [
+
+  "Same here, dude.",
+
+  "That's cool! Go on...",
+
+  "Dude...",
+
+  "Ask something else...",
+
+  "Hey, I'm listening..."
+
+];
+
+ 
+
+const synth = window.speechSynthesis;
+
+ 
+
+function voiceControl(string) {
+
+  let u = new SpeechSynthesisUtterance(string);
+
+  u.text = string;
+
+  u.lang = "en-aus";
+
+  u.volume = 1;
+
+  u.rate = 1;
+
+  u.pitch = 1;
+
+  synth.speak(u);
+
 }
 
-const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
+ 
 
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
+function sendMessage() {
+
+  const inputField = document.getElementById("input");
+
+  let input = inputField.value.trim();
+
+  input != "" && output(input);
+
+  inputField.value = "";
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const inputField = document.getElementById("input");
+
+  inputField.addEventListener("keydown", function (e) {
+
+    if (e.code === "Enter") {
+
+      let input = inputField.value.trim();
+
+      input != "" && output(input);
+
+      inputField.value = "";
+
     }
 
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+  });
 
-const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
-
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-    
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
-}
-
-chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
-chatInput.addEventListener("keydown", (e) => {
-    // If Enter key is pressed without Shift key and the window 
-    // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat();
+ 
+
+function output(input) {
+
+  let product;
+
+ 
+
+  let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+ 
+
+  text = text
+
+    .replace(/[\W_]/g, " ")
+
+    .replace(/ a /g, " ")
+
+    .replace(/i feel /g, "")
+
+    .replace(/whats/g, "what is")
+
+    .replace(/please /g, "")
+
+    .replace(/ please/g, "")
+
+    .trim();
+
+ 
+
+  let comparedText = compare(userMessage, botReply, text);
+
+ 
+
+  product = comparedText
+
+    ? comparedText
+
+    : alternative[Math.floor(Math.random() * alternative.length)];
+
+  addChat(input, product);
+
+}
+
+ 
+
+function compare(triggerArray, replyArray, string) {
+
+  let item;
+
+  for (let x = 0; x < triggerArray.length; x++) {
+
+    for (let y = 0; y < replyArray.length; y++) {
+
+      if (triggerArray[x][y] == string) {
+
+        items = replyArray[x];
+
+        item = items[Math.floor(Math.random() * items.length)];
+
+      }
+
     }
-});
 
-sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+  }
+
+  //containMessageCheck(string);
+
+  if (item) return item;
+
+  else return containMessageCheck(string);
+
+}
+
+ 
+
+function containMessageCheck(string) {
+
+  let expectedReply = [
+
+    [
+
+      "Good Bye, dude",
+
+      "Bye, See you!",
+
+      "Dude, Bye. Take care of your health in this situation."
+
+    ],
+
+    ["Good Night, dude", "Have a sound sleep", "Sweet dreams"],
+
+    ["Have a pleasant evening!", "Good evening too", "Evening!"],
+
+    ["Good morning, Have a great day!", "Morning, dude!"],
+
+    ["Good Afternoon", "Noon, dude!", "Afternoon, dude!"]
+
+  ];
+
+  let expectedMessage = [
+
+    ["bye", "tc", "take care"],
+
+    ["night", "good night"],
+
+    ["evening", "good evening"],
+
+    ["morning", "good morning"],
+
+    ["noon"]
+
+  ];
+
+  let item;
+
+  for (let x = 0; x < expectedMessage.length; x++) {
+
+    if (expectedMessage[x].includes(string)) {
+
+      items = expectedReply[x];
+
+      item = items[Math.floor(Math.random() * items.length)];
+
+    }
+
+  }
+
+  return item;
+
+}
+
+function addChat(input, product) {
+
+  const mainDiv = document.getElementById("message-section");
+
+  let userDiv = document.createElement("div");
+
+userDiv.id = "user";
+
+  userDiv.classList.add("message");
+
+  userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+
+  mainDiv.appendChild(userDiv);
+
+ 
+
+  let botDiv = document.createElement("div");
+
+botDiv.id = "bot";
+
+  botDiv.classList.add("message");
+
+  botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
+
+  mainDiv.appendChild(botDiv);
+
+  var scroll = document.getElementById("message-section");
+
+  scroll.scrollTop = scroll.scrollHeight;
+
+  voiceControl(product);
+
+}
